f641ba1b3fe3345bb2cf66510b3297db06da6682
Nelson Acosta <nelson.acosta.web@gmail.com>
2025-02-27 16:04:21 -0300

    test: add tests for RecommendedPopup component
    - Verify rendering of recommended products.
    - Ensure onClose is called when clicking the X button.

--- src/product/__mocks__/productsMock.ts
+++ src/product/__mocks__/productsMock.ts
@@ -38,5 +38,80 @@ export const mockProducts: Product[] = [
         discount_percentage: 15,
         size: 500,
         price_per_litre: "25",
-    }
+    },
+    {
+        "product_id": "4854058319917",
+        "variant_id": "33619202211885",
+        "total_price": "283.50",
+        "price_per_unit": "283.5",
+        "list_price_id": "7",
+        "sku": "Combo Gaseosas Latas 354",
+        "categories": [
+            "gaseosas"
+        ],
+        "units_per_pack": 1,
+        "image_url": "https://cdn.shopify.com/s/files/1/0257/2242/1293/products/Combo_Gaseosa_Latas.jpg?v=1585767272",
+        "handle": "7up-light-paso-de-los-toros-pomelo-pepsi-black-6pack-354ml-por-variedad",
+        "compare_at_price": "630.00",
+        "allowed_packs": [
+            1,
+            2,
+            3
+        ],
+        "name": "7up Light & Paso de los toros Pomelo & Pepsi Black. (6pack 354ml por variedad)",
+        "description": "",
+        "discount_percentage": 55.00000000000001,
+        "size": 1000,
+        "price_per_litre": "201"
+    },
+    {
+        "product_id": "4854058647597",
+        "variant_id": "33619204177965",
+        "total_price": "806.40",
+        "price_per_unit": "806.4",
+        "list_price_id": "7",
+        "sku": "Combo Patagonia Latas 473",
+        "categories": [
+            "cervezas"
+        ],
+        "units_per_pack": 1,
+        "image_url": "https://cdn.shopify.com/s/files/1/0257/2242/1293/products/Combo_Patagonia_Latas.jpg?v=1585767274",
+        "handle": "copy-of-patagonia-710ml-hoppy-lager-ipa-24-7-bohemian-pilsener-1",
+        "compare_at_price": "2016.00",
+        "allowed_packs": [
+            1,
+            2,
+            3
+        ],
+        "name": "Incluye un 6pack por variedad de: IPA 24.7; Amber Lager; Weisse. Total 18 Latas",
+        "description": "",
+        "discount_percentage": 60,
+        "size": 1000,
+        "price_per_litre": "201"
+    },
+    {
+        "product_id": "4854058942509",
+        "variant_id": "33619205554221",
+        "total_price": "501.60",
+        "price_per_unit": "501.6",
+        "list_price_id": "7",
+        "sku": "Combo Porron 330",
+        "categories": [
+            "cervezas"
+        ],
+        "units_per_pack": 1,
+        "image_url": "https://cdn.shopify.com/s/files/1/0257/2242/1293/products/Combo_Porron.jpg?v=1585767275",
+        "handle": "copy-of-patagonia-710ml-hoppy-lager-ipa-24-7-bohemian-pilsener",
+        "compare_at_price": "912.00",
+        "allowed_packs": [
+            1,
+            2,
+            3
+        ],
+        "name": "Incluye un 6pack de Corona y uno de Stella Artois. Total 12",
+        "description": "Botellas",
+        "discount_percentage": 44.99999999999999,
+        "size": 1000,
+        "price_per_litre": "201"
+    },
 ];
